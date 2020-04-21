3c476c35296eeb60627715bf1ead4e9f0c6e3650
Lourence John <digicono.recruitment@gmail.com>
2020-04-22 07:41:04 +0800

    layout qa form

--- resources/views/auditor.blade.php
+++ resources/views/auditor.blade.php
@@ -1,9 +1,27 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 
 @section('content')
 
-<h3>Hi Auditor</h3>
+<div style="width: 1600px">
+	<div class="f1-grid">
+		<div class="row-1-col-1"></div>
+		<div class="row-1-col-2">Customer</div>
+		<div class="row-1-col-3">Agent</div>
+		<div class="row-1-col-4">information</div>
+		<div class="row-1-col-5">Comment</div>
+		<div class="row-2-col-1">Tone</div>
+		<div class="row-2-col-2">Acknowledgemment</div>	
+		<div class="row-2-col-3">Accuracy</div>
+		<div class="row-3-col-1">Statement (question/objection)</div>
+		<div class="row-3-col-2">Agent response</div>
+		<div class="row-3-col-3">Speed</div>
+		<div class="row-3-col-4">Correct response</div>
+		<div class="row-3-col-5">Customer details</div>
+		<div class="row-3-col-6">Agent input</div>
+		<div class="row-3-col-7">Call ends</div>
+	</div>
+</div>
 
 @endsection
 
